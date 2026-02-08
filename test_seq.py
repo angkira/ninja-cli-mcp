9524c5ae28bda9565b7831e08774eff61e2b25a8
Angkira <angkira0@gmail.com>
2026-02-08 13:19:22 +0100

    [ninja-auto-save] Before task: Create test_seq.py with a Math class that has an __init__ me...
    Timestamp: 2026-02-08 13:19:22
    Automatic safety commit by ninja-coder

---
+++ test_seq.py
@@ -0,0 +1,5 @@
+[
+    'Union[int, float]] = None) -> None:\n        "',
+    '\n        Initialize a Math instance.\n\n        Args:\n            value: An optional initial value. Can be an integer, float, or None.\n                   Defaults to None.\n\n        Examples:\n            >>> m = Math()\n            >>> m.value\n            >>> m = Math(10)\n            >>> m.value\n            10\n            >>> m = Math(3.14)\n            >>> m.value\n            3.14\n        "',
+    "\n        self.value = value",
+]
